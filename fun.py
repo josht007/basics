8d46826fe790a63cfb05dd7213af23948a6fd61d
Joshua Tabazi <148504666+josht007@users.noreply.github.com>
2024-08-11 23:49:31 -0400

    Update fun.py

--- fun.py
+++ fun.py
@@ -1,9 +1,17 @@
 import random
 
-digit = random.randint(1, 200)
+nba_players = ["LeBron James", "Kevin Durant", "Stephen Curry", "Kawhi Leonard", "Giannis Antetokounmpo"]
 
-if input("Guess \n") == digit:
-    print("Correct")
-else:
-    print("Nope")
 
+selected_player = random.choice(nba_players)
+
+while True:
+    
+    guess = input("Guess the NBA player: ")
+
+    
+    if guess == selected_player:
+        print("Congratulations! You guessed correctly.")
+        break
+    else:
+        print("Sorry, that's not correct. Try again.")
